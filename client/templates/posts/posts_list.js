aea7b0eb2f95192766f8b199f3d47bd979c8fa0a
ojenks_dev <ojenkins@imarketsmart.com>
2015-08-13 13:11:12 -0400

    Added basic posts list template and static data

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,23 @@
+var postsData = [
+	{
+		title: 'Introducing Telescope', 
+		url: 'http://sachagrief.com/introducing-telescope/'
+
+	}, 
+
+	{
+		title: 'Meteor', 
+		url: 'http://meteor.com'
+	}, 
+
+	{
+		title: 'The Meteor Book', 
+		url: 'http://themeteorbook.com'
+	}
+
+
+]; 
+
+Template.postsList.helpers({
+	posts: postsData
+}); 
